0c9eb9b125d84e4f3a7a81524ddf1dc5015be3ed
Jansiya jahan <jansiyajahan8@gmail.com>
2025-03-04 12:04:59 +0530

    feat: add new About us session

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,21 @@
+import Achievements from "./Components/Achievements"
+import HeroSection from "./Components/HeroSection"
+import Mission from "./Components/Mission"
+import OurStory from "./Components/OurStory"
+import TeamIntro from "./Components/TeamIntro"
+
+
+const About = () => {
+  return (
+     <div >
+          <HeroSection />
+          <OurStory />
+          <Mission />
+          <TeamIntro />
+          <Achievements/>
+   
+    </div>
+  )
+}
+
+export default About
