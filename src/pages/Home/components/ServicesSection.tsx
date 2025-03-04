c87e8a401a8b121e2274181832351d87a3c65af4
Jansiya jahan <jansiyajahan8@gmail.com>
2025-03-04 12:18:36 +0530

    refactor: separate data into  component

--- src/pages/Home/components/ServicesSection.tsx
+++ src/pages/Home/components/ServicesSection.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { SlideData } from '../../../interfaces/Pages';
 import { Link } from 'react-router-dom';
 import FRONTEND_ROUTES from '../../../Routes/frontendRoutes';
+import { services } from '../../../Data/homeData';
 
 
 
-const services: SlideData[] = [
-  {
-    title: 'Hair Styling',
-    description: 'Trendy cuts, blowouts, and updos for any occasion.',
-    image: 'https://saloonsite.s3.eu-north-1.amazonaws.com/WhatsApp+Image+2025-02-21+at+14.00.22_f6ad545b.jpg',
-  },
-  {
-    title: 'Hair Coloring',
-    description: 'Vibrant colors and professional highlights for a fresh look.',
-     image: 'https://saloonsite.s3.eu-north-1.amazonaws.com/WhatsApp+Image+2025-02-22+at+10.29.49_8754802f.jpg',
-      },
-  {
-    title: 'Spa Treatments',
-    description: 'Rejuvenate with relaxing facials, massages, and more.',
-    image: 'https://saloonsite.s3.eu-north-1.amazonaws.com/wellness-concept-with-woman-massage-salon.jpg',
-  },
-  {
-    title: 'Manicure & Pedicure',
-    description: 'Pamper yourself with our nail care and polish services.',
-    image: 'https://saloonsite.s3.eu-north-1.amazonaws.com/nail-hygiene-care-nail-polish.jpg',
-  },
-];
+
 
 const containerVariants = {
   hidden: { opacity: 0 },
