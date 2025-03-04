e73bff2a316d8ad10c78c482b17907f2360602cc
Jansiya jahan <jansiyajahan8@gmail.com>
2025-03-04 12:28:56 +0530

    feat(routes): set up React Router for frontend navigation

--- src/Routes/frontendRoutes.ts
+++ src/Routes/frontendRoutes.ts
@@ -1,10 +1,15 @@
 const FRONTEND_ROUTES = {
   HOME: "/",
+  ABOUT:"/about",
   SERVICES: "/services",
   BOOKING: "/booking",
+  CONTACT:"/contact",
+  FORGOT_PASSWORD:'/forget_password',
   LOGIN: "/login",
   REGISTER: "/register",
   NOT_FOUND: "*", 
+  APPOINTMENT: "/appointment",
+  BLOG:"/blog"
 };
 
 export default FRONTEND_ROUTES;
